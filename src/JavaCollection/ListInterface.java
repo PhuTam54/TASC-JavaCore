c365d146d3d51ededd77faf3950189e31d504ad5
PhuTam54 <phutamytb@gmail.com>
2024-04-25 17:39:24 +0700

    Add more code demo about Stack

--- src/JavaCollection/ListInterface.java
+++ src/JavaCollection/ListInterface.java
@@ -1,9 +1,6 @@
 package JavaCollection;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.*;
 
 public class ListInterface {
     public static void main(String[] args) {
@@ -12,13 +9,17 @@ public class ListInterface {
         arrayList.add("Apple");
         arrayList.add("Banana");
         arrayList.add(1, "Cherry");
+        arrayList.get(1);
+        arrayList.set(1, "Date");
+        boolean isEmpty = arrayList.isEmpty();
         boolean isContain = arrayList.contains("Banana");
         int listSize = arrayList.size();
         arrayList.iterator().forEachRemaining(System.out::println);
         arrayList.lastIndexOf("Banana");
         arrayList.remove(1);
         arrayList.listIterator().forEachRemaining(System.out::println);
-        System.out.println("ArrayList: " + arrayList + isContain + listSize);
+        arrayList.clear();
+        System.out.println("ArrayList: " + arrayList + isContain + isEmpty + listSize);
 
         // LinkedList
         LinkedList<String> linkedList = new LinkedList<>();
@@ -53,5 +54,36 @@ public class ListInterface {
         String lastInFirstOutItem = stack.pop();
         String lastInFirstOutItemWithOutRemove = stack.peek();
         System.out.println("Stack: " + stack + lastInFirstOutItem + lastInFirstOutItemWithOutRemove);
+        Scanner sc = new Scanner(System.in);
+
+        Stack<String> stackString = new Stack<>();
+
+        // example of reversing a string
+        System.out.println("Enter the string: ");
+        String s = sc.nextLine();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            stackString.push(s.charAt(i) + "");
+        }
+        System.out.println("Reversal string: ");
+        for (int i = 0; i < len; i++) {
+            System.out.print(stackString.pop());
+        }
+
+        // example of converting from decimal to binary
+        Stack<String> stackPositiveNumber = new Stack<>();
+        System.out.println("\nEnter a positive integer:");
+        int x = sc.nextInt();
+        while (x > 0) {
+            int positiveNumber = x % 2;
+            System.out.print(positiveNumber);
+            stackPositiveNumber.push(positiveNumber + "");
+            x /= 2;
+        }
+        System.out.println("\nBinary number is:");
+        int n = stackPositiveNumber.size();
+        for (int i = 0; i < n; i++) {
+            System.out.print(stackPositiveNumber.pop());
+        }
     }
 }
---
+++ src/JavaCollection/QueueAndDequeInterface.java
@@ -0,0 +1,5 @@
+package JavaCollection;
+
+public class QueueAndDequeueInterface {
+
+}
